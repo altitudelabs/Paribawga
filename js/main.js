c1c5cc0674a36334f5603e5145aeabf350366597
Kathleen Sucipto <sucipto.kathleen@gmail.com>
2016-10-05 15:00:41 +0800

    Change collection content

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
-  // NAV MENU
+  /* * * * * * * * * * * *
+   *  NAV MENU
+   * * * * * * * * * * * */
   var isSubNav = $('.nav-menu').hasClass('hidden')? true: false;
 
   function showMenu(){
@@ -24,11 +26,26 @@ $(document).ready(function(){
   $('.nav-menu .title').one("click", showMenu);
   $('.header .title').one("click", showMenu);
 
-  if ($(window).innerWidth() >= 1024) {
-    $('.nav-menu .links').css('display', 'block');
-  } else {
-    $('.nav-menu .links').css('display', 'none');
-  }
+  $(window).resize($.throttle(250, function(){
+    if ($(window).innerWidth() >= 1024) {
+      $('.nav-menu .links').css('display', 'block');
+      console.log("hey");
+    } else {
+      $('.nav-menu .links').css('display', 'none');
+    }
+  }));
 
+  $(window).scroll($.throttle(250, function() {
+    var winScrollTop = $(window).scrollTop();
+    var winWidth = $(window).innerWidth();
+
+    if (winWidth >= 1024) {
+      if (winScrollTop >= 600) { // 1st section-height
+        $('.nav-menu').css('color', 'black');
+      } else {
+        $('.nav-menu').css('color', 'white');
+      }
+    }
+  }));
 
 });
