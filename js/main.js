0bcd6969efd1f3121312e66d62480f66d147e41a
Kathleen Sucipto <sucipto.kathleen@gmail.com>
2016-10-06 17:09:00 +0800

    Add friction

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
   /* * * * * * * * * * * *
-   *  NAV MENU
-   * * * * * * * * * * * */
+  *  NAV MENU
+  * * * * * * * * * * * */
   var isSubNav = $('.nav-menu').hasClass('hidden')? true: false;
 
   function showMenu(){
@@ -56,8 +56,63 @@ $(document).ready(function(){
         $('.index #footer').addClass('hidden');
       }
     } else {
-      $('#footer').removeClass('hidden');
+      $('.index #footer').removeClass('hidden');
     }
   }));
+
+  /* * * * * * * * * * * *
+  *  ANIMATION
+  * * * * * * * * * * * */
+
+  // Friction
+
+  var fricFastElement = $('.frictionFast'),
+      fricMedElement = $('.frictionMed'),
+      fricSlowElement = $('.frictionSlow');
+
+  // distance = const * delta in the checkScrollSpeed function
+  var distanceFast = 5,
+      distanceMed = -5, // add (-) just to make it different~
+      distanceSlow = 7;
+
+  var checkScrollSpeed = (function(settings){
+    settings = settings || {};
+
+    var lastPos, newPos, timer, delta,
+    delay = settings.delay || 50; // in "ms" (higher means lower fidelity )
+
+    // set back the velocity to 0
+    function clear() {
+      lastPos = null;
+      delta = 0;
+      fricFastElement.css("transform", "none");
+      fricMedElement.css("transform", "none");
+      fricSlowElement.css("transform", "none");
+    }
+
+    clear();
+
+    return function(){
+      newPos = window.scrollY;
+      if ( lastPos != null ){ // && newPos < maxScroll
+        delta = newPos -  lastPos;
+
+        var velocityFast = delta * distanceFast,
+            velocityMed = delta * distanceMed,
+            velocitySlow = delta * distanceSlow;
+
+        fricFastElement.css("transform", "translateY(" + velocityFast.toString() + "px)");
+        fricMedElement.css("transform", "translateY(" + velocityMed.toString() + "px)");
+        fricSlowElement.css("transform", "translateY(" + velocitySlow.toString() + "px)");
+
+      }
+      lastPos = newPos;
+      clearTimeout(timer);
+      timer = setTimeout(clear, delay);
+      return delta;
+    };
+  })();
+
+  window.onscroll = checkScrollSpeed;
 
 });
